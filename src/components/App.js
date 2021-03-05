a20167bde57ff0f0783696c82f8bcdc6f67ee294
Richeese <rricheese@users.noreply.github.com>
2021-03-05 08:37:44 +0800

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -31,7 +31,7 @@ class DrumPad extends React.Component {
     }
 
     handleKeyPress(event) {
-        if(event.keyCode == this.props.item.keyCode){
+        if(event.keyCode === this.props.item.keyCode){
             this.playSound();
         }
     }
@@ -85,13 +85,28 @@ class App extends React.Component {
 
     changeVolume(event) {
         this.setState({
-            volume: event.target.value
+            volume: event.target.value,
+            display: 'Volume: ' + event.target.value
         });
     }
 
     handleKeyPress(event) {
         if(event.keyCode === 16){
             this.bankSwitch();
+        }
+        if(event.keyCode === 82){
+            const newVolume = this.state.volume >= 95 ? 100 : this.state.volume + 5;
+            this.setState({
+                volume: newVolume,
+                display: 'Volume: ' + newVolume
+            });
+        }
+        if(event.keyCode === 70){
+            const newVolume = this.state.volume <= 5 ? 0 : this.state.volume - 5;
+            this.setState({
+                volume: newVolume,
+                display: 'Volume: ' + newVolume
+            });
         }
     }
 
@@ -135,10 +150,11 @@ class App extends React.Component {
     }
 
     render() {
+        [...document.getElementsByTagName('audio')].forEach(audio => audio.volume = this.state.volume / 100);
         return (
             <div id="drum-machine">
                 <div id="drum-container">
-                    { banks[this.state.currentBank].map((item, index) => <DrumPad item={item} updateDisplay={this.changeDisplay} index={index}/>) }
+                    { banks[this.state.currentBank].map((item, index) => <DrumPad item={item} updateDisplay={this.changeDisplay} index={index} />) }
                 </div>
                 <div id="display-container">
                     <h3 style={{color: 'white', marginBottom: '20px'}}>Drum Machine</h3>
@@ -151,7 +167,11 @@ class App extends React.Component {
                             LSHIFT
                         </button>
                     </div>
-                    <input type="range" min="1" max="100" onChange={this.changeVolume} value={this.state.volume}></input>
+                    <div style={{display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center'}}>
+                        <p style={{color: 'white', margin: '0px', fontWeight: '700'}}>F - </p>
+                        <input type="range" min="0" max="100" onChange={this.changeVolume} value={this.state.volume}></input>
+                        <p style={{color: 'white', margin: '0px', fontWeight: '700'}}> + R</p>
+                    </div>
                 </div>
             </div>
         );
